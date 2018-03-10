787e8031f66b90267c4d692d718f9ed0765a0907
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2018-03-10 17:05:23 +0530

    Update both; cleanup; rm funcs from the .h and mv to the .c too..

--- convenient.h
+++ convenient.h
@@ -44,63 +44,63 @@
 #undef USE_FTRACE_PRINT
 
 #ifdef USE_FTRACE_PRINT
- #define DBGPRINT(string, args...) \
+#define DBGPRINT(string, args...) \
      trace_printk(string, ##args);
 #else
- #define DBGPRINT(string, args...) do {                 \
-     int USE_RATELIMITING=0;                            \
-	 /* Not supposed to use printk_ratelimit() now.. */ \
-	 if (USE_RATELIMITING) {                            \
-        printk_ratelimited (KERN_INFO pr_fmt(string), ##args);  \
-	 }                                                  \
-	 else                                               \
-        printk (KERN_INFO pr_fmt(string), ##args);              \
- } while (0)
+#define DBGPRINT(string, args...) do {                             \
+     int USE_RATELIMITING=0;                                       \
+	 if (USE_RATELIMITING) {                                   \
+	   printk_ratelimited (KERN_INFO pr_fmt(string), ##args);  \
+	 }                                                         \
+	 else                                                      \
+           pr_info(pr_fmt(string), ##args);                        \
+} while (0)
 #endif
 
 /*------------------------ MSG, QP ------------------------------------*/
 #ifdef DEBUG
 #ifdef __KERNEL__
 #define MSG(string, args...) do {                                     \
-		 DBGPRINT("%s:%d : " string, __FUNCTION__, __LINE__, ##args);      \
-	 } while (0)
+	DBGPRINT("%s:%d : " string, __FUNCTION__, __LINE__, ##args);  \
+} while (0)
 #else
-#define MSG(string, args...) do {                                      \
-		fprintf(stderr, "%s:%d : " string, __FUNCTION__, __LINE__, ##args); \
-	 } while (0)
+#define MSG(string, args...) do {                                           \
+	fprintf(stderr, "%s:%d : " string, __FUNCTION__, __LINE__, ##args); \
+} while (0)
 #endif
 
 #ifdef __KERNEL__
-#define MSG_SHORT(string, args...) do { \
-			DBGPRINT(string, ##args);        \
-	 } while (0)
+#define MSG_SHORT(string, args...) do {  \
+	DBGPRINT(string, ##args);        \
+} while (0)
 #else
-#define MSG_SHORT(string, args...) do { \
-			fprintf(stderr, string, ##args); \
-	 } while (0)
+#define MSG_SHORT(string, args...) do {  \
+	fprintf(stderr, string, ##args); \
+} while (0)
 #endif
 
 // QP = Quick Print
 #define QP MSG("\n")
 
 #ifdef __KERNEL__
-#define QPDS do { \
-		 MSG("\n");    \
-		 dump_stack(); \
-	  } while(0)
+#define QPDS do {     \
+	MSG("\n");    \
+	dump_stack(); \
+} while(0)
 #endif
 
 #ifdef __KERNEL__
 #define HexDump(from_addr, len) \
  	    print_hex_dump_bytes (" ", DUMP_PREFIX_ADDRESS, from_addr, len);
 #endif
-#else
+#else				/* #ifdef DEBUG */
 #define MSG(string, args...)
 #define MSG_SHORT(string, args...)
 #define QP
 #define QPDS
 #endif
 
+//#ifdef __KERNEL__
 /*------------------------ PRINT_CTX ---------------------------------*/
 /* 
  An interesting way to print the context info:
@@ -153,20 +153,22 @@
 	);                                                                                   \
 } while (0)
 #else				// using ftrace trace_prink() internally
-#define PRINT_CTX() do {                                                                   \
-	DBGPRINT("PRINT_CTX:: [cpu %02d]%s:%d\n", smp_processor_id(), __func__, current->pid); \
-	if (!in_interrupt()) {                                                                 \
-  		DBGPRINT(" in process context:%c%s%c:%d\n",                                        \
-		    (!current->mm?'[':' '), current->comm, (!current->mm?']':' '), current->pid);  \
-	} else {                                                                               \
-        DBGPRINT(" in interrupt context: in_interrupt:%3s. in_irq:%3s. in_softirq:%3s. "   \
-		"in_serving_softirq:%3s. preempt_count=0x%x\n",                                    \
-          (in_interrupt()?"yes":"no"), (in_irq()?"yes":"no"), (in_softirq()?"yes":"no"),   \
-          (in_serving_softirq()?"yes":"no"), (preempt_count() && 0xff));                   \
-	}                                                                                      \
+#define PRINT_CTX() do {                                                                     \
+	DBGPRINT("PRINT_CTX:: [cpu %02d]%s:%d\n", smp_processor_id(), __func__,              \
+			current->pid);                                                       \
+	if (!in_interrupt()) {                                                               \
+  		DBGPRINT(" in process context:%c%s%c:%d\n",                                  \
+		    (!current->mm?'[':' '), current->comm, (!current->mm?']':' '),           \
+				current->pid);                                               \
+	} else {                                                                             \
+        DBGPRINT(" in interrupt context: in_interrupt:%3s. in_irq:%3s. in_softirq:%3s. "     \
+		"in_serving_softirq:%3s. preempt_count=0x%x\n",                              \
+          (in_interrupt()?"yes":"no"), (in_irq()?"yes":"no"), (in_softirq()?"yes":"no"),     \
+          (in_serving_softirq()?"yes":"no"), (preempt_count() && 0xff));                     \
+	}                                                                                    \
 } while (0)
 #endif
-#endif				// end ifdef __KERNEL__ at the top
+#endif				/* end ifdef __KERNEL__ at the top */
 
 /*------------------------ assert ---------------------------------------
  * Hey you, careful! 
@@ -204,18 +206,18 @@ static inline void beep(int what)
  * @val : ASCII value to print
  * @loop_count : times to loop around
  */
-#define DELAY_LOOP(val,loop_count) \
-{ \
-	int c=0, m;\
-	unsigned int for_index,inner_index; \
-	\
-	for(for_index=0;for_index<loop_count;for_index++) { \
-		beep((val)); \
-		c++;\
+#define DELAY_LOOP(val,loop_count)                                             \
+{                                                                              \
+	int c=0, m;                                                            \
+	unsigned int for_index,inner_index;                                    \
+	                                                                       \
+	for(for_index=0;for_index<loop_count;for_index++) {                    \
+		beep((val));                                                   \
+		c++;                                                           \
 			for(inner_index=0;inner_index<HZ*1000*8;inner_index++) \
-				for(m=0;m<50;m++); \
-		} \
-	/*printf("c=%d\n",c);*/\
+				for(m=0;m<50;m++);                             \
+		}                                                              \
+	/*printf("c=%d\n",c);*/                                                \
 }
 /*------------------------------------------------------------------------*/
 
@@ -224,179 +226,13 @@ static inline void beep(int what)
  * Delays execution for n seconds.
  * MUST be called from process context.
  *------------------------------------------------*/
-#define DELAY_SEC(val) \
-{ \
-	if (!in_interrupt()) {	\
+#define DELAY_SEC(val)                                  \
+{                                                       \
+	if (!in_interrupt()) {	                        \
 		set_current_state (TASK_INTERRUPTIBLE); \
-		schedule_timeout (val * HZ); \
-	}	\
+		schedule_timeout (val * HZ);            \
+	}	                                        \
 }
-#endif
-
-#ifndef __KERNEL__
-/* Get time difference between two struct timeval's
- * Credits: Arkaitz Jimenez
- * http://stackoverflow.com/questions/1444428/time-stamp-in-the-c-programming-language
- */
-//#include <sys/time.h>
-static int timeval_subtract(struct timeval *result, struct timeval *x,
-			    struct timeval *y);
-/* Subtract the `struct timeval' values X and Y,
-    storing the result in RESULT.
-    Return 1 if the difference is negative, otherwise 0.  */
-__attribute__ ((unused))
-int timeval_subtract(struct timeval *result, struct timeval *x,
-		     struct timeval *y)
-{
-	/* Perform the carry for the later subtraction by updating y. */
-	if (x->tv_usec < y->tv_usec) {
-		int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-		y->tv_usec -= 1000000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_usec - y->tv_usec > 1000000) {
-		int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-		y->tv_usec += 1000000 * nsec;
-		y->tv_sec -= nsec;
-	}
-
-	/* Compute the time remaining to wait.
-	   tv_usec is certainly positive. */
-	result->tv_sec = x->tv_sec - y->tv_sec;
-	result->tv_usec = x->tv_usec - y->tv_usec;
-
-	/* Return 1 if result is negative. */
-	return x->tv_sec < y->tv_sec;
-}
-
-/* 
- * function r _ s l e e p
- *
- * Wrapper around nanosleep(2) , and in such a way that interruption 
- * due to a non-blocked signals causes restart.
- *
- * Note: this actually belongs in a "restart library" header..
- */
-#ifndef _TIME_H
-#include <time.h>
-#endif
-#include <errno.h>
-int r_sleep (unsigned int sec)
-{
-	struct timespec req, rem;
-
-	req.tv_sec = sec;
-	req.tv_nsec = 0;
-	//while( (nanosleep (&req, &rem) == -1) && (errno == EINTR)) {
-	while (nanosleep (&req, &rem) == -1) {
-		if (errno != EINTR)
-			return -1;
-		/* Actually should not use [f]printf in sig handler - not aysnc-signal 
-		 * safe - just shown for demo purpose here (works with DEBUG option on). Ugly!
-		 */
-#ifdef DEBUG
-		fprintf (stderr, "* nanosleep interrupted! rem time: %lu.%lu *\n",
-			rem.tv_sec, rem.tv_nsec);
-#endif
-	  	req = rem;
-	}
-	return 0;
-}
-
-#endif
-
-/*
- * Converts decimal to binary. 
- * Credits: vegaseat. URL: http://www.daniweb.com/software-development/c/code/216349
- * accepts a decimal integer and returns a binary coded string
- *
- * @decimal : decimal value to convert to binary (IN)
- * @binary  : the binary result as a string (OUT)
- *
- */
-__attribute__ ((unused))
-static void dec2bin(long decimal, char *binary)
-{
-	int k = 0, n = 0;
-	int neg_flag = 0;
-	int remain;
-	/*
-	   gcc 4.6.3 : we get the warning:
-	   "warning: variable ‘old_decimal’ set but not used [-Wunused-but-set-variable]"
-	   To get rid of this warning, have #ifdef'd the test... -kaiwan.
-
-	   Keep one of the following below (wrt TESTMODE); comment out the other.
-	   UN-defining by default.
-	 */
-//#define TESTMODE
-#undef TESTMODE
-
-#ifdef TESTMODE
-	int old_decimal;	// for test
-#endif
-	char temp[80];
-
-	// take care of negative input
-	if (decimal < 0) {
-		decimal = -decimal;
-		neg_flag = 1;
-	}
-	do {
-#ifdef TESTMODE
-		old_decimal = decimal;	// for test
-#endif
-		remain = decimal % 2;
-		// whittle down the decimal number
-		decimal = decimal / 2;
-		// this is a test to show the action
-#ifdef TESTMODE
-		printf("%d/2 = %d  remainder = %d\n", old_decimal, decimal,
-		       remain);
-#endif
-		// converts digit 0 or 1 to character '0' or '1'
-		temp[k++] = remain + '0';
-	} while (decimal > 0);
-
-	if (neg_flag)
-		temp[k++] = '-';	// add - sign
-	else
-		temp[k++] = ' ';	// space
-
-	// reverse the spelling
-	while (k >= 0)
-		binary[n++] = temp[--k];
-
-	binary[n - 1] = 0;	// end with NULL
-}
-
-#ifndef __KERNEL__
-
-#define MSG(string, args...) do {                                      \
-	fprintf(stderr, "%s:%d : " string, __FUNCTION__, __LINE__, ##args); \
-} while (0)
-
-#define MSG_SHORT(string, args...) do { \
-		fprintf(stderr, string, ##args); \
-} while (0)
-
-#define QP MSG("\n")
-
-/* Verbose printf ... */
-#define VP(verbose, str, args...) \
-do { \
-	if (verbose) \
-		printf(str, ##args); \
-} while (0)
-
-static inline int err_exit(char *prg, char *err, int exitcode)
-{
-	char err_str[512];
-
-	snprintf(err_str, 511, "%s: %s", prg, err);
-	perror(err_str);
-	exit(exitcode);
-}				// err_exit()
-
 #endif
 
 #endif
