10c55f6c6f3079ab8f8fc77a3e7a5823fbf0b0c8
kaiwan nb <kaiwan.billimoria@gmail.com>
2023-10-23 12:17:59 +0530

    Take into a/c differing nmap scan report formats to get devname

--- localnet_discov.sh
+++ localnet_discov.sh
@@ -19,7 +19,7 @@ discover_devices_local()
 {
 #local tmpfile=$(mktemp)
 local tmpfile=/tmp/ssh2seccam0100 # $(tempfile)
-local rec macaddr line=1 iplinenum ipline ipaddr devname
+local rec macaddr numcol line=1 iplinenum ipline ipaddr devname
 #sudo arp-scan --localnet|grep "^192[^\(DUP:]" > ${tmp}
 #sudo arp-scan --localnet|grep "^192" > ${tmp}
 
@@ -28,6 +28,12 @@ local rec macaddr line=1 iplinenum ipline ipaddr devname
 	return
 }
 #local start=$(echo $1 | cut -d. -f1)
+
+# $1 : IP addr
+get_mac_from_ip()
+{
+  ip a |grep -B2 $1 |grep "link/ether "|awk '{print $2}'
+}
 
 #---
 # Local net Host Discovery:
@@ -49,9 +55,10 @@ local rec macaddr line=1 iplinenum ipline ipaddr devname
 # (Just doing nmap -h shows useful stuff!)
 #sudo arp-scan $1/24 | grep "^${start}" > ${tmp}
 
+GATEWAY_IP=$1
 sudo rm -f ${tmpfile}
 sudo touch ${tmpfile}
-sudo nmap -sn -PR $1/24 --host-timeout 1m -oN ${tmpfile} > /dev/null
+sudo nmap -sn -PR ${GATEWAY_IP}/24 --host-timeout 1m -oN ${tmpfile} > /dev/null
 [ $? -ne 0 ] && {
 	echo "nmap failed; stat=$?" ; return
 }
@@ -66,18 +73,40 @@ decho() {
 [[ ${DEBUG} -eq 1 ]] && echo $*
 }
 
+#--- for debug
 DEBUG=0
 #set -x
+#tmpfile=~/usefulsnips/t192dot
+#tmpfile=~/usefulsnips/t10dot
+#---
+
 IFS=$'\n'
 for rec in $(cat ${tmpfile})
 #for rec in $(cat t1)
 do
   decho "rec= $rec"
+  GOT_MACADDR=0
+  GOT_DEVNAME=0
+  macaddr=""
+  devname=""
+  ipaddr=""
+
+  # nmap o/p differs depending on the subnet??
+  # For 192.168.1.0 I get 5 col o/p NOT including the device name
+  #  Eg. 'Nmap scan report for 192.168.1.10'
+  # For   10.20.1.0 I get 6 col o/p including the device name (col 5)
+  #  Eg. 'Nmap scan report for yeelink-light-lamp4_mibt4AC6.wlan (10.20.1.15)'
+  # Check this and accordingly get the device name ...
+ 
+  # Get MAC address
   echo "${rec}" |grep "^MAC Address:" >/dev/null && {
 	macaddr=$(echo "${rec}" |awk '{print $3}')
+	GOT_MACADDR=1
+
 	devname=$(echo "${rec}" |awk -F\( '{print $2}')
 	devname=${devname::-1} # rm the trailing ')'
-	echo -n "${macaddr},"
+	[[ ! -z "${devname}" ]] && GOT_DEVNAME=1
+
 	#echo -n "${macaddr},\"${devname}\","
 	# 2 lines above this record is the line containing the IP addr
 	iplinenum=$((line-2))
@@ -92,13 +121,11 @@ do
 	decho "
 ipline=$ipline
 	"
-	#continue
 
 	echo "${ipline}" |grep -E -q "\(.*\)" # esc the () when using egrep, not with grep! # ?
 	if [ $? -eq 0 ] ; then  # IP addr is in parentheses: 
 			# Of the form:
 			#  Nmap scan report for _gateway (192.168.1.1)
-	#	devname=$(echo "${ipline}"|awk '{print $5}')
 		ipaddr=$(echo "${ipline}"|awk '{print $6}')
 		ipaddr=${ipaddr:1}   # rm the leading '('
 		ipaddr=${ipaddr::-1} # rm the trailing ')'
@@ -106,8 +133,46 @@ ipline=$ipline
 		# Of the form: 'Nmap scan report for 192.168.1.8'
 		ipaddr=$(echo "${ipline}"|awk '{print $5}')
 	fi
-	echo "${devname}, ${ipaddr}"
+	#[[ -z "${ipaddr}" ]] && [[ "${devname}" = "_gateway" ]] && echo -n "${GATEWAY_IP},"
 	}
+
+	# Under the '^MAC Address:' line, look for 'Nmap scan report for ...' and
+	# get the device name there
+	echo "${rec}" |grep "^Nmap scan report for" >/dev/null && {
+		numcol=$(echo "${rec}" |grep "^Nmap scan report for " |awk '{print NF}')
+		decho "numcol = ${numcol}"
+
+		# Eg. 'Nmap scan report for yeelink-light-lamp4_mibt4AC6.wlan (10.20.1.15)'
+
+		# Get the device name
+		[[ ${GOT_DEVNAME} -eq 1 ]] && continue
+		[[ ${numcol} -eq 6 ]] && {
+			devname=$(echo "${rec}" |awk '{print $5}')
+			#devname=${devname::-1} # rm the trailing ')'
+
+			# Get the device IP addr
+			echo "${rec}" |grep -E -q "\(.*\)"
+			if [ $? -eq 0 ] ; then  # IP addr is in parentheses: 
+				# Of the form:
+				#  Nmap scan report for _gateway (192.168.1.1)
+				ipaddr=$(echo "${rec}"|awk '{print $6}')
+				ipaddr=${ipaddr:1}   # rm the leading '('
+				ipaddr=${ipaddr::-1} # rm the trailing ')'
+			else
+				ipaddr=$(echo "${rec}"|awk '{print $6}')
+			fi
+		}
+	}
+  decho "line=$line: GOT_MACADDR = ${GOT_MACADDR} ***"
+  [[ ${GOT_MACADDR} -eq 1 ]] && echo -n "${macaddr},"  || {
+	if [[ ! -z "${ipaddr}" ]] && [[ ! -z "${devname}" ]] ; then
+	   macaddr=$(get_mac_from_ip ${ipaddr})
+	fi
+	[[ ! -z "${macaddr}" ]] && echo -n "${macaddr},"
+  }
+  [[ ! -z "${devname}" ]] && echo -n "${devname}, "
+  [[ ! -z "${ipaddr}" ]] && echo "${ipaddr}"
+
   let line=line+1
 done
 }
